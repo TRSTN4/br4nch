1ca251a84bb3ffb818087d726d48d765799820af
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-03-15 15:50:09 +0100

    indev-v1.0.3
    Checks and adds branch.

--- br4nch/br4nch.py
+++ br4nch/br4nch.py
@@ -1,22 +1,25 @@
-# Indev phase - br4nch v1.0.2
-# desc - Introducing executor. It executes simpler actions like creating a branch, getting basic information etc.
+# Indev phase - br4nch v1.0.3
+# desc - Checks and adds branch.
 
 
 def executor(action):
+    global new_branch
+
     if action == "new_branch":
         new_branch = []
+        return new_branch
 
     if action == "get_branch":
         return new_branch
 
 
 def create_branch(branch_name):
-    executor("new_branch").append(branch_name)
-    print(executor("get_branch"))
+    add_name = executor("new_branch")
+    add_name.append(branch_name)
 
 
 def create_header(branch_name, header_name):
-    pass
+    check_existing_branch("my_branch")
 
 
 def create_module(branch_name, module_name):
@@ -31,17 +34,15 @@ def create_object(branch_name, module_name, subject_name, object_name):
     pass
 
 
-def check_existing(branch_name):
-
+def check_existing_branch(branch_name):
     if branch_name in executor("get_branch"):
-        print(executor("get_branch"))
+        print("yes")
     else:
-        executor("get_branch").append(branch_name)
+        print("no")
 
-
-create_branch("test")
 
 create_branch("my_branch")
+
 create_header("my_branch", "just a header")
 create_module("my_branch", "sample header")
 create_subject("my_branch", "sample header", "some kind of subject")
