7d29a62bbb2f715a8f25b4fe341efbee86862efb
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-03-15 15:46:32 +0100

    indev-v1.0.1
    Adding test functions and test args.

--- br4nch/br4nch.py
+++ br4nch/br4nch.py
@@ -1,4 +1,35 @@
-# Indev phase - br4nch v1.0.0
-# desc - Hello world!
+# Indev phase - br4nch v1.0.1
+# desc - Adding test functions and test args.
 
-print("Hello world!")
+
+def create_branch(branch_name):
+    pass
+
+
+def create_header(branch_name, header_name):
+    pass
+
+
+def create_module(branch_name, module_name):
+    pass
+
+
+def create_subject(branch_name, module_name, subject_name):
+    pass
+
+
+def create_object(branch_name, module_name, subject_name, object_name):
+    pass
+
+
+create_branch("my_branch")
+create_header("my_branch", "just a header")
+create_module("my_branch", "sample header")
+create_subject("my_branch", "sample header", "some kind of subject")
+create_object("my_branch", "sample header", "some kind of subject", "this is a object")
+
+# br4nch.create(arg1)
+# br4nch.header(arg1, arg2)
+# br4nch.module(arg1, arg2)
+# br4nch.subject(arg1, arg2, arg3)
+# br4nch.object(arg1, arg2, arg3, arg4)
