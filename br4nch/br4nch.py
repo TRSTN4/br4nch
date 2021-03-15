d1553fd5a2290fe84882ed5bdd1969d563707242
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-03-15 15:54:15 +0100

    indev-v1.0.6
    First prototype of the structure and testing structure with sample text.

--- br4nch/br4nch.py
+++ br4nch/br4nch.py
@@ -1,19 +1,19 @@
-# Indev phase - br4nch v1.0.5
-# desc - Working with dictionaries for better structure.
+# Indev phase - br4nch v1.0.6
+# desc - First prototype of the structure and testing structure with sample text.
 
 
-def executor(action):
-    global branch
+def testing_structure():
+    get_branch = executor("get_branch")
 
-    if action == "new_branch":
-        branch = {}
-
-    if action == "get_branch":
-        return branch
-
-
-def initiate():
-    executor("new_branch")
+    for name in get_branch:
+        for header in get_branch[name]:
+            print("", header)
+            for module in get_branch[name][header]:
+                print(" ", module)
+                for subject in get_branch[name][header][module]:
+                    print("  ", subject)
+                    for obj in get_branch[name][header][module][subject]:
+                        print("   ", obj)
 
 
 def create_name(branch_name):
@@ -39,7 +39,6 @@ def create_subject(branch_name, header_name, module_name, subject_name):
 def create_object(branch_name, header_name, module_name, subject_name, object_name):
     get_branch = executor("get_branch")
     get_branch[branch_name][header_name][module_name][subject_name].update({object_name: {}})
-    print(get_branch)
 
 
 def check_existing_branch(branch_name):
@@ -49,17 +48,39 @@ def check_existing_branch(branch_name):
         return False
 
 
+def executor(action):
+    global branch
+
+    if action == "new_branch":
+        branch = {}
+
+    if action == "get_branch":
+        return branch
+
+
+def initiate():
+    executor("new_branch")
+
+
 if __name__ == '__main__':
     initiate()
 
-    create_name("my_branch")
-    create_header("my_branch", "just a header")
-    create_module("my_branch", "just a header", "sample header")
-    create_subject("my_branch", "just a header", "sample header", "some kind of subject")
-    create_object("my_branch", "just a header", "sample header", "some kind of subject", "this is a object")
+    create_name("Computer Branch")
+    create_header("Computer Branch", "Gaming")
+    create_module("Computer Branch", "Gaming", "Monitors")
+    create_module("Computer Branch", "Gaming", "Keyboard & Mouse")
+    create_subject("Computer Branch", "Gaming", "Monitors", "LG")
+    create_subject("Computer Branch", "Gaming", "Keyboard & Mouse", "Steel Series")
+    create_subject("Computer Branch", "Gaming", "Keyboard & Mouse", "Razer")
+    create_object("Computer Branch", "Gaming", "Monitors", "LG", "LG 27GN850 Ultragear")
+    create_object("Computer Branch", "Gaming", "Keyboard & Mouse", "Razer", "Razer Blackwindow Elite")
+    create_object("Computer Branch", "Gaming", "Keyboard & Mouse", "Steel Series", "SteelSeries Apex 3")
+    create_object("Computer Branch", "Gaming", "Keyboard & Mouse", "Steel Series", "SteelSeries Apex 5")
 
-# br4nch.create(arg1)
-# br4nch.header(arg1, arg2)
-# br4nch.module(arg1, arg2, arg3)
-# br4nch.subject(arg1, arg2, arg3, arg4)
-# br4nch.object(arg1, arg2, arg3, arg4, arg5)
+    testing_structure()
+
+# br4nch.add.branch(arg1)
+# br4nch.add.header(arg1, arg2)
+# br4nch.add.module(arg1, arg2, arg3)
+# br4nch.add.subject(arg1, arg2, arg3, arg4)
+# br4nch.add.object(arg1, arg2, arg3, arg4, arg5)
