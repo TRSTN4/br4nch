e6a04fac1075ac93fe546aa11113218d642d121f
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-03-15 15:48:42 +0100

    indev-v1.0.2
    Introducing executor. It executes simpler actions like creating a branch, getting basic information etc.

--- br4nch/br4nch.py
+++ br4nch/br4nch.py
@@ -1,9 +1,18 @@
-# Indev phase - br4nch v1.0.1
-# desc - Adding test functions and test args.
+# Indev phase - br4nch v1.0.2
+# desc - Introducing executor. It executes simpler actions like creating a branch, getting basic information etc.
+
+
+def executor(action):
+    if action == "new_branch":
+        new_branch = []
+
+    if action == "get_branch":
+        return new_branch
 
 
 def create_branch(branch_name):
-    pass
+    executor("new_branch").append(branch_name)
+    print(executor("get_branch"))
 
 
 def create_header(branch_name, header_name):
@@ -21,6 +30,16 @@ def create_subject(branch_name, module_name, subject_name):
 def create_object(branch_name, module_name, subject_name, object_name):
     pass
 
+
+def check_existing(branch_name):
+
+    if branch_name in executor("get_branch"):
+        print(executor("get_branch"))
+    else:
+        executor("get_branch").append(branch_name)
+
+
+create_branch("test")
 
 create_branch("my_branch")
 create_header("my_branch", "just a header")
