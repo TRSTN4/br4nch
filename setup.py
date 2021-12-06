881c476b0992a120918f3f939e5ce23fb4e7f0aa
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-12-06 21:37:20 +0100

    setup.py version change.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name="br4nch",
-    version="1.0.0",
+    version="1.0",
     description="br4nch is a data structure tree generator for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
