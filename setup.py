b08985ac390cacaa0de4c5ae2a282ddaf8df2014
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-12-06 21:30:04 +0100

    Added long description setup.py.

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
+from os import path
 from setuptools import setup, find_packages
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
 
 setup(
     name="br4nch",
     version="1.0.0",
     description="br4nch is a data structure tree generator for Python.",
-    url="https://br4nch.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TRSTN4/br4nch",
     author="TRSTN4",
     author_email="<tristan@trstn4.com>",
     classifiers=[
         "Intended Audience :: Developers",
         "Development Status :: 5 - Production/Stable",
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'],
+        "Programming Language :: Python :: 3.9"],
     keywords=["br4nch", "python", "python3", "data", "structure", "tree", "generate", "algorithm"],
     license="GNU General Public License v3.0",
     download_url="https://github.com/TRSTN4/br4nch/archive/refs/tags/v1.0.0.tar.gz",
