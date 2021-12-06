b4be9ad677da7e9275b0a155a104559d41d95f32
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-12-06 21:20:44 +0100

    Removed setup.py classifiers.

--- setup.py
+++ setup.py
@@ -8,16 +8,13 @@ setup(
     author="TRSTN4",
     author_email="<tristan@trstn4.com>",
     classifiers=[
+        "Intended Audience :: Developers",
         "Development Status :: 5 - Production/Stable",
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        "Intended Audience :: Developers"
-        "Operating System :: Unix"
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows"],
+        'Programming Language :: Python :: 3.9'],
     keywords=["br4nch", "python", "python3", "data", "structure", "tree", "generate", "algorithm"],
     license="GNU General Public License v3.0",
     download_url="https://github.com/TRSTN4/br4nch/archive/refs/tags/v1.0.0.tar.gz",
