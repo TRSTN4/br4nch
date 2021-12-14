8d71b482bffbe0686175829a7956018fe09507b8
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2021-12-14 13:54:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name="br4nch",
-    version="1.0.1",
+    version="1.0.2",
     description="br4nch is a data structure tree generator for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,7 +19,7 @@ setup(
         "Programming Language :: Python :: 3.9"],
     keywords=["br4nch", "python", "python3", "data", "structure", "tree", "generate", "algorithm"],
     license="GNU General Public License v3.0",
-    download_url="https://github.com/TRSTN4/br4nch/archive/refs/tags/1.0.1.tar.gz",
+    download_url="https://github.com/TRSTN4/br4nch/archive/refs/tags/1.0.2.tar.gz",
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3'
