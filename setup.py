f849615e880e6c71b29bdbb6ac4e727096de4385
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2022-01-09 10:45:55 +0100

    Renamed 'Documentation' folder to 'docs'.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from os import path
 from setuptools import setup, find_packages
 
+version = "1.1"
+
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
     name="br4nch",
-    version="1.1",
+    version=version,
     description="br4nch is a data structure tree generator for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,7 +21,7 @@ setup(
         "Programming Language :: Python :: 3.9"],
     keywords=["br4nch", "python", "python3", "data", "structure", "tree", "generate", "algorithm"],
     license="GNU General Public License v3.0",
-    download_url="https://github.com/TRSTN4/br4nch/archive/refs/tags/1.1.tar.gz",
+    download_url="https://github.com/TRSTN4/br4nch/archive/refs/tags/" + version + ".tar.gz",
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3'
