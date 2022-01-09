9f034759fb0b26718730f86d9560763b8beb0720
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2022-01-09 23:56:13 +0100

    Updated copyright year to 2022.

--- br4nch/set/set_size.py
+++ br4nch/set/set_size.py
@@ -1,4 +1,4 @@
-# Copyright 2021 by TRSTN4. All rights reserved.
+# Copyright 2021-2022 by TRSTN4. All rights reserved.
 # This file is part of the br4nch python package, and is released under the "GNU General Public License v3.0".
 # Please see the LICENSE file that should have been included as part of this package.
 
