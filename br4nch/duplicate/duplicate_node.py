b565a82909b2a1ecdbf89362ab1497d5d279b3cb
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2022-04-13 23:11:25 +0200

    Rewritten 'duplicate' functions.

---
+++ br4nch/duplicate/duplicate_node.py
@@ -0,0 +1,143 @@
+# br4nch - Data Structure Tree Builder
+# Author: https://TRSTN4.com
+# Website: https://br4nch.com
+# Documentation: https://docs.br4nch.com
+# Github Repository: https://github.com/TRSTN4/br4nch
+
+import copy
+
+from br4nch.utility.utility_librarian import existing_trees, existing_uids, existing_paint_nodes
+from br4nch.utility.utility_handler import InstanceBooleanError, InstanceStringError, NotExistingTreeError
+from br4nch.utility.utility_generator import generate_uid
+from br4nch.utility.utility_positioner import format_position
+
+
+class DuplicateNode:
+    def __init__(self, tree, node, parent, sibling="", attributes=False, delete=False):
+        self.trees = tree
+        self.nodes = node
+        self.parents = parent
+        self.siblings = sibling
+        self.attributes = attributes
+        self.delete = delete
+
+        self.validate_arguments()
+        self.duplicate_node()
+
+    def validate_arguments(self):
+        if not isinstance(self.trees, list):
+            self.trees = [self.trees]
+
+        if not isinstance(self.nodes, list):
+            self.nodes = [self.nodes]
+
+        if not isinstance(self.parents, list):
+            self.parents = [self.parents]
+
+        if not isinstance(self.siblings, list):
+            self.siblings = [self.siblings]
+
+        if not isinstance(self.attributes, bool):
+            raise InstanceBooleanError("attributes", self.attributes)
+
+        if not isinstance(self.delete, bool):
+            raise InstanceBooleanError("delete", self.delete)
+
+        for trees in [self.trees + self.siblings]:
+            for index in range(len(trees)):
+                if not isinstance(trees[index], str):
+                    raise InstanceStringError("tree", trees[index])
+
+                if trees[index] not in list(map(str.lower, existing_trees)):
+                    raise NotExistingTreeError(trees[index])
+
+                for existing_tree in list(map(str.lower, existing_trees)):
+                    if trees[index].lower() == existing_tree.lower():
+                        trees[index] = existing_tree
+
+            if "*" in trees:
+                trees.clear()
+                for existing_tree in list(existing_trees):
+                    trees.append(existing_tree)
+
+    def duplicate_node(self):
+        for tree in self.trees:
+            queue_delete = []
+            queue_add = []
+
+            for node in format_position(tree, self.nodes.copy()):
+                for parent in format_position(tree, self.parents.copy()):
+                    children = self.get_nodes(tree, node, [], existing_trees[tree][list(existing_trees[tree])[0]])
+
+                    if children:
+                        queue_delete.append(children[1])
+
+                        for sibling in self.siblings:
+                            queue_add.append([children[0], self.get_nodes(sibling, [], parent,
+                                                                          existing_trees[sibling]
+                                                                          [list(existing_trees[sibling])[0]])])
+
+            if self.delete:
+                for delete_node in queue_delete:
+                    if delete_node:
+                        for parent_node, child_nodes in delete_node.items():
+                            existing_uids[tree].remove(str(parent_node[-10:]))
+                            existing_paint_nodes[tree].pop(parent_node)
+
+                            self.delete_node_attributes(tree, child_nodes[parent_node])
+                            del child_nodes[parent_node]
+
+            for add_node in queue_add:
+                if add_node and add_node[0] and add_node[1]:
+                    self.change_node_uid(list(add_node[1])[0], add_node[0])
+                    add_node[1][list(add_node[1])[0]].update(add_node[0])
+
+    def get_nodes(self, tree, node, parent, child):
+        if parent and parent[0] == "0":
+            return {tree: existing_trees[tree][list(existing_trees[tree])[0]]}
+
+        count = 0
+        for parent_node, child_nodes in child.items():
+            count = count + 1
+
+            if node and count == int(node[0]):
+                if len(node) == 1:
+                    return [{parent_node: copy.deepcopy(child_nodes)}, {parent_node: child}]
+                else:
+                    if child_nodes:
+                        node.pop(0)
+                        return self.get_nodes(tree, node, parent, child_nodes)
+
+            if parent and count == int(parent[0]):
+                if len(parent) == 1:
+                    return {tree: child_nodes}
+                else:
+                    if child_nodes:
+                        parent.pop(0)
+                        return self.get_nodes(tree, node, parent, child_nodes)
+
+    def delete_node_attributes(self, tree, child):
+        for parent_node, child_nodes in child.items():
+            if parent_node[-10:] in existing_uids[tree]:
+                existing_uids[tree].remove(str(parent_node[-10:]))
+
+            if parent_node in existing_paint_nodes[tree]:
+                existing_paint_nodes[tree].pop(parent_node)
+
+            if child_nodes:
+                self.delete_node_attributes(tree, child_nodes)
+
+    def change_node_uid(self, tree, child):
+        for parent_node, child_nodes in child.copy().items():
+            parent_node_uid = parent_node[:-15] + generate_uid(tree)
+
+            child[parent_node_uid] = child.pop(parent_node)
+
+            if parent_node in existing_paint_nodes[tree]:
+                if self.attributes:
+                    existing_paint_nodes[tree].update({parent_node_uid: existing_paint_nodes[tree][parent_node]})
+            else:
+                existing_paint_nodes[tree].update({parent_node_uid: []})
+
+            if child_nodes:
+                self.change_node_uid(tree, child_nodes)
---
+++ br4nch/duplicate/duplicate_tree.py
@@ -0,0 +1,63 @@
+# br4nch - Data Structure Tree Builder
+# Author: https://TRSTN4.com
+# Website: https://br4nch.com
+# Documentation: https://docs.br4nch.com
+# Github Repository: https://github.com/TRSTN4/br4nch
+
+from br4nch.utility.utility_librarian import existing_trees, existing_output, existing_uids, existing_sizes, \
+    existing_symbols, existing_paint_trees, existing_paint_headers, existing_paint_nodes
+from br4nch.utility.utility_handler import InstanceStringError, InstanceBooleanError, InvalidTreeNameError, \
+    DuplicateTreeError, NotExistingTreeError
+
+
+class DuplicateTree:
+    def __init__(self, tree, sibling, attributes=False):
+        self.tree = tree
+        self.sibling = sibling
+        self.attributes = attributes
+
+        self.validate_arguments()
+        self.duplicate_tree()
+
+    def validate_arguments(self):
+        if not isinstance(self.tree, str):
+            raise InstanceStringError("tree", self.tree)
+
+        if not self.tree.isalnum():
+            raise InvalidTreeNameError(self.tree)
+
+        for existing_tree in list(existing_trees):
+            if self.tree.lower() == existing_tree.lower():
+                raise DuplicateTreeError(self.tree)
+
+        if not isinstance(self.sibling, str):
+            raise InstanceStringError("sibling", self.sibling)
+
+        if self.sibling not in list(map(str.lower, existing_trees)):
+            raise NotExistingTreeError(self.sibling)
+
+        for existing_tree in list(map(str.lower, existing_trees)):
+            if self.sibling.lower() == existing_tree.lower():
+                self.sibling = existing_tree
+
+        if not isinstance(self.attributes, bool):
+            raise InstanceBooleanError("attributes", self.attributes)
+
+    def duplicate_tree(self):
+        existing_trees.update({self.tree: existing_trees[self.sibling]})
+        existing_output.update({self.tree: []})
+
+        if self.attributes:
+            existing_uids.update({self.tree: existing_uids[self.sibling]})
+            existing_sizes.update({self.tree: existing_sizes[self.sibling]})
+            existing_symbols.update({self.tree: existing_symbols[self.sibling]})
+            existing_paint_trees.update({self.tree: existing_paint_trees[self.sibling]})
+            existing_paint_headers.update({self.tree: existing_paint_headers[self.sibling]})
+            existing_paint_nodes.update({self.tree: existing_paint_nodes[self.sibling]})
+        else:
+            existing_uids.update({self.tree: []})
+            existing_sizes.update({self.tree: 0})
+            existing_symbols.update({self.tree: {"line": "┃", "split": "┣━", "end": "┗━"}})
+            existing_paint_trees.update({self.tree: []})
+            existing_paint_headers.update({self.tree: []})
+            existing_paint_nodes.update({self.tree: {}})
