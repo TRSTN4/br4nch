ae11fe34689486412e48ce688d6da77795c2f50c
Tristan <trstnvb4@gmail.com>
2022-04-19 17:54:33 +0200

    Rewritten "CheckVersion" algorithm.

--- br4nch/__init__.py
+++ br4nch/__init__.py
@@ -5,10 +5,12 @@
 # Github Repository: https://github.com/TRSTN4/br4nch
 
 __version__ = "1.2.1"
-__author__ = "TRSTN4"
-__author_email__ = "tristan@trstn4.com"
 __description__ = "Data Structure Tree Builder"
 __url__ = "https://github.com/TRSTN4/br4nch"
+
+__author__ = "TRSTN4"
+__author_email__ = "tristan@trstn4.com"
+__author_website__ = "https://TRSTN4.com"
 
 import os
 import imp
@@ -30,57 +32,69 @@ import br4nch.display
 import br4nch.utility
 
 
-def check_version():
-    if not os.path.isfile(imp.find_module('br4nch')[1] + "/update_check.br4nch"):
-        create_update_check_file()
+class CheckVersion:
+    def __init__(self):
+        self.path = imp.find_module('br4nch')[1] + "/update_check.br4nch"
 
-    update_check_file = open(imp.find_module('br4nch')[1] + "/update_check.br4nch", "rt")
+        self.check_version()
 
-    file_data = update_check_file.readlines()
+    def check_version(self):
+        if not os.path.isfile(self.path):
+            self.create_update_check_file()
 
-    if file_data[0][8:-1] != __version__ and file_data[0][8:-1] != "None":
-        file_data[0] = file_data[0].replace(file_data[0][8:-1], "None")
-        file_data[1] = file_data[1].replace(file_data[1][16:-1], "None")
-        file_data[2] = file_data[2].replace(file_data[2][17:-1], "None")
-        file_data[3] = file_data[3].replace(file_data[3][17:], "None")
+        update_check_file = open(self.path, "rt")
+        file = update_check_file.readlines()
 
-    if file_data[0][8:-1] == "None":
-        file_data[0] = file_data[0].replace(file_data[0][8:-1], __version__)
+        file_version = file[0]
+        file_last_check_date = file[1]
+        file_skip_check_hours = file[2]
+        file_update_available = file[3]
 
-    if str(file_data[1][16:-1]) == "None" or datetime.now().strftime("%H") not in file_data[2][17:-1].split(",") or \
-            datetime.now().strftime("%H") in file_data[2][17:-1].split(",") and \
-            date.today().strftime("%d") != file_data[1][16:-1]:
-        file_data[1] = file_data[1].replace(file_data[1][16:-1], date.today().strftime("%d"))
+        if file_version[8:-1] != __version__ and file_version[8:-1] != "None":
+            file_version = file_version.replace(file_version[8:-1], "None")
+            file_last_check_date = file_last_check_date.replace(file_last_check_date[16:-1], "None")
+            file_skip_check_hours = file_skip_check_hours.replace(file_skip_check_hours[17:-1], "None")
+            file_update_available = file_update_available.replace(file_update_available[17:], "None")
 
-        skip_check = []
-        for hour in range(4):
-            skip_check.append((datetime.strptime(
-                datetime.now().strftime("%H"), "%H") + timedelta(hours=hour)).strftime("%H"))
-        file_data[2] = file_data[2].replace(file_data[2][17:-1], ",".join(skip_check))
+        if file_version[8:-1] == "None":
+            file_version = file_version.replace(file_version[8:-1], __version__)
 
-        file_data[3] = file_data[3].replace(file_data[3][17:], json.loads(urllib.request.urlopen(
-            "https://api.github.com/repos/TRSTN4/br4nch/releases/latest").read())["tag_name"])
+        if str(file_last_check_date[16:-1]) == "None" or datetime.now().strftime("%H") \
+                not in file_skip_check_hours[17:-1].split(",") or datetime.now().strftime("%H") \
+                in file_skip_check_hours[17:-1].split(",") \
+                and date.today().strftime("%d") != file_last_check_date[16:-1]:
+            file_last_check_date = file_last_check_date.replace(
+                file_last_check_date[16:-1], date.today().strftime("%d"))
 
-    if file_data[3][17:] != __version__:
-        print("[!] There is a new br4nch version (" + file_data[3][17:] + ") available, The current installed br4nch "
-              + "version (" + __version__ + ") is outdated.")
+            skip_check = []
+            for hour in range(4):
+                skip_check.append((datetime.strptime(
+                    datetime.now().strftime("%H"), "%H") + timedelta(hours=hour)).strftime("%H"))
+            file_skip_check_hours = file_skip_check_hours.replace(file_skip_check_hours[17:-1], ",".join(skip_check))
 
-    update_check_file.close()
+            file_update_available = file_update_available.replace(file_update_available[17:], json.loads(
+                urllib.request.urlopen(
+                    "https://api.github.com/repos/TRSTN4/br4nch/releases/latest").read())["tag_name"])
 
-    update_check_file = open(imp.find_module('br4nch')[1] + "/update_check.br4nch", "wt")
-    update_check_file.write(file_data[0] + file_data[1] + file_data[2] + file_data[3])
-    update_check_file.close()
+        if file_update_available[17:] != __version__:
+            print("[!] There is a new br4nch version (" + file_update_available[17:] + ") available, The current installed br4nch "
+                  + "version (" + __version__ + ") is outdated.")
 
+        update_check_file.close()
 
-def create_update_check_file():
-    with open(imp.find_module('br4nch')[1] + "/update_check.br4nch", 'w', encoding='utf-8') as file:
-        file.write("version=None\n")
-        file.write("last-check-date=None\n")
-        file.write("skip-check-hours=None\n")
-        file.write("update-available=None")
+        update_check_file = open(self.path, "wt")
+        update_check_file.write(file_version + file_last_check_date + file_skip_check_hours + file_update_available)
+        update_check_file.close()
+
+    def create_update_check_file(self):
+        with open(self.path, 'w', encoding='utf-8') as file:
+            file.write("version=None\n")
+            file.write("last-check-date=None\n")
+            file.write("skip-check-hours=None\n")
+            file.write("update-available=None")
 
 
 try:
-    check_version()
+    CheckVersion()
 except:
     pass
