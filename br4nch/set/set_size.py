cd59648ba64e033b1d6cbf4af411b600a7b43ef0
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2022-05-10 12:13:18 +0200

    Documented the 'size' features.

--- br4nch/set/set_size.py
+++ br4nch/set/set_size.py
@@ -10,6 +10,14 @@ from ..utility.utility_handler import UtilityHandler
 
 class SetSize:
     def __init__(self, tree, size):
+        """
+        Required argument(s):
+        - tree
+        - size
+
+        :param tree: The tree(s) where the symbols are added.
+        :param size: The size of the space in the tree structure.
+        """
         self.trees = tree
         self.size = size
 
@@ -17,29 +25,39 @@ class SetSize:
         self.set_size()
 
     def validate_arguments(self):
+        """
+        Validates the arguments.
+        """
+        # If the value is not an instance of a list, set the value in the list.
         if not isinstance(self.trees, list):
             self.trees = [self.trees]
 
+        # If there is a '*' in the tree value, add all existing trees to the list.
         if "*" in self.trees:
             self.trees.clear()
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 self.trees.append(existing_tree)
 
         for index in range(len(self.trees)):
+            # Raises an error when the tree value is not a string.
             if not isinstance(self.trees[index], str):
                 raise UtilityHandler.InstanceStringError("tree", self.trees[index])
 
+            # Raises an error when the given tree does not exist.
             if self.trees[index].lower() not in list(map(str.lower, UtilityLibrarian.existing_trees)):
                 raise UtilityHandler.NotExistingTreeError(self.trees[index])
 
+            # Sets the tree to the exact tree name.
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 if self.trees[index].lower() == existing_tree.lower():
                     self.trees[index] = existing_tree
 
+        # Raises an error when the size value is not an int.
         if not isinstance(self.size, int):
             raise UtilityHandler.InstanceIntegerError("size", self.size)
 
         for tree in self.trees:
+            # Raises an error if the symbols are changes and are not the default symbols.
             if UtilityLibrarian.existing_symbols[tree]["line"] != "┃":
                 raise UtilityHandler.NotSizeableError
             if UtilityLibrarian.existing_symbols[tree]["split"] != "┣━":
@@ -47,14 +65,18 @@ class SetSize:
             if UtilityLibrarian.existing_symbols[tree]["end"] != "┗━":
                 raise UtilityHandler.NotSizeableError
 
+        # Raises an error if the given size is lower than '0' and bigger than '20'.
         if self.size < 0 or self.size > 20:
             raise UtilityHandler.InvalidSizeError
 
     def set_size(self):
+        """
+        Sets the size.
+        """
         for tree in self.trees:
-            if UtilityLibrarian.existing_symbols[tree]["split"] == "┣━":
-                UtilityLibrarian.existing_symbols[tree]["split"] = "┣━" + "━" * self.size
-            if UtilityLibrarian.existing_symbols[tree]["end"] == "┗━":
-                UtilityLibrarian.existing_symbols[tree]["end"] = "┗━" + "━" * self.size
+            # Changes each the size of the 'split' and 'end' symbols.
+            UtilityLibrarian.existing_symbols[tree]["split"] = "┣━" + "━" * self.size
+            UtilityLibrarian.existing_symbols[tree]["end"] = "┗━" + "━" * self.size
 
+            # Sets the size.
             UtilityLibrarian.existing_sizes.update({tree: self.size})
--- br4nch/set/set_symbol.py
+++ br4nch/set/set_symbol.py
@@ -10,6 +10,20 @@ from ..utility.utility_handler import UtilityHandler
 
 class SetSymbol:
     def __init__(self, tree, line="", split="", end=""):
+        """
+        Required argument(s):
+        - tree
+
+        Optional argument(s):
+        - line
+        - split
+        - end
+
+        :param tree: The tree where the symbols are set.
+        :param line: The line symbol.
+        :param split: The split symbol.
+        :param end: The end symbol.
+        """
         self.trees = tree
         self.line = line
         self.split = split
@@ -19,42 +33,58 @@ class SetSymbol:
         self.set_symbol()
 
     def validate_arguments(self):
+        """
+        Validates the arguments.
+        """
+        # If the value is not an instance of a list, set the value in the list.
         if not isinstance(self.trees, list):
             self.trees = [self.trees]
 
+        # If there is a '*' in the tree value, add all existing trees to the list.
         if "*" in self.trees:
             self.trees.clear()
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 self.trees.append(existing_tree)
 
         for index in range(len(self.trees)):
+            # Raises an error when the tree value is not a string.
             if not isinstance(self.trees[index], str):
                 raise UtilityHandler.InstanceStringError("tree", self.trees[index])
 
+            # Raises an error when the given tree does not exist.
             if self.trees[index].lower() not in list(map(str.lower, UtilityLibrarian.existing_trees)):
                 raise UtilityHandler.NotExistingTreeError(self.trees[index])
 
+            # Sets the tree to the exact tree name.
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 if self.trees[index].lower() == existing_tree.lower():
                     self.trees[index] = existing_tree
 
         if self.line:
+            # Raises an error when the line value is not a string.
             if not isinstance(self.line, str):
                 raise UtilityHandler.InstanceStringError("line", self.line)
 
         if self.split:
+            # Raises an error when the split value is not a string.
             if not isinstance(self.split, str):
                 raise UtilityHandler.InstanceStringError("split", self.split)
 
         if self.end:
+            # Raises an error when the end value is not a string.
             if not isinstance(self.end, str):
                 raise UtilityHandler.InstanceStringError("end", self.end)
 
+        # Raises an error when there are no symbol changes.
         if not self.line and not self.split and not self.end:
             raise UtilityHandler.RequiredSymbolChangeError
 
     def set_symbol(self):
+        """
+        Sets each symbol.
+        """
         for tree in self.trees:
+            # Gets each old symbol if that symbol has not changed.
             if not self.line:
                 self.line = UtilityLibrarian.existing_symbols[tree]["line"]
             if not self.split:
@@ -62,4 +92,5 @@ class SetSymbol:
             if not self.end:
                 self.end = UtilityLibrarian.existing_symbols[tree]["end"]
 
+            # Sets the symbols.
             UtilityLibrarian.existing_symbols[tree].update({"line": self.line, "split": self.split, "end": self.end})
