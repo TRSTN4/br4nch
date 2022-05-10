04e93ee2b1524e258d5511742a004e26c36226c7
TRSTN4 <56277690+TRSTN4@users.noreply.github.com>
2022-05-10 12:47:38 +0200

    Documented the 'reset' features.

--- br4nch/reset/reset_size.py
+++ br4nch/reset/reset_size.py
@@ -10,32 +10,50 @@ from ..utility.utility_handler import UtilityHandler
 
 class ResetSize:
     def __init__(self, tree):
+        """
+        Required argument(s):
+        - tree
+
+        :param tree: The tree(s) whose size should be reset.
+        """
         self.trees = tree
 
         self.validate_arguments()
         self.reset_size()
 
     def validate_arguments(self):
+        """
+        Validates the arguments.
+        """
+        # If the value is not an instance of a list, set the value in the list.
         if not isinstance(self.trees, list):
             self.trees = [self.trees]
 
+        # If there is a '*' in the tree value, add all existing trees to the list.
         if "*" in self.trees:
             self.trees.clear()
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 self.trees.append(existing_tree)
 
         for index in range(len(self.trees)):
+            # Raises an error when the tree value is not a string.
             if not isinstance(self.trees[index], str):
                 raise UtilityHandler.InstanceStringError("tree", self.trees[index])
 
+            # Raises an error when the given tree does not exist.
             if self.trees[index].lower() not in list(map(str.lower, UtilityLibrarian.existing_trees)):
                 raise UtilityHandler.NotExistingTreeError(self.trees[index])
 
+            # Sets the tree to the exact tree name.
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 if self.trees[index].lower() == existing_tree.lower():
                     self.trees[index] = existing_tree
 
     def reset_size(self):
+        """
+        Resets the size.
+        """
         for tree in self.trees:
+            # Resets to the default size value and the default symbols.
             UtilityLibrarian.existing_sizes.update({tree: 0})
             UtilityLibrarian.existing_symbols.update({tree: {"line": "┃", "split": "┣━", "end": "┗━"}})
--- br4nch/reset/reset_symbol.py
+++ br4nch/reset/reset_symbol.py
@@ -10,6 +10,20 @@ from ..utility.utility_handler import UtilityHandler
 
 class ResetSymbol:
     def __init__(self, tree, line=True, split=True, end=True):
+        """
+        Required argument(s):
+        - tree
+
+        Optional argument(s):
+        - line
+        - split
+        - end
+
+        :param tree: The tree(s) whose symbol(s) should be reset.
+        :param line: If 'True', the line symbol is reset to the default line symbol.
+        :param split: If 'True', the split symbol is reset to the default split symbol.
+        :param end: If 'True', the end symbol is reset to the default end symbol.
+        """
         self.trees = tree
         self.line = line
         self.split = split
@@ -19,39 +33,54 @@ class ResetSymbol:
         self.reset_symbol()
 
     def validate_arguments(self):
+        """
+        Validates the arguments.
+        """
+        # If the value is not an instance of a list, set the value in the list.
         if not isinstance(self.trees, list):
             self.trees = [self.trees]
 
+        # If there is a '*' in the tree value, add all existing trees to the list.
         if "*" in self.trees:
             self.trees.clear()
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 self.trees.append(existing_tree)
 
         for index in range(len(self.trees)):
+            # Raises an error when the tree value is not a string.
             if not isinstance(self.trees[index], str):
                 raise UtilityHandler.InstanceStringError("tree", self.trees[index])
 
+            # Raises an error when the given tree does not exist.
             if self.trees[index].lower() not in list(map(str.lower, UtilityLibrarian.existing_trees)):
                 raise UtilityHandler.NotExistingTreeError(self.trees[index])
 
+            # Sets the tree to the exact tree name.
             for existing_tree in list(UtilityLibrarian.existing_trees):
                 if self.trees[index].lower() == existing_tree.lower():
                     self.trees[index] = existing_tree
 
         if self.line:
+            # Raises an error when the line value is not a bool.
             if not isinstance(self.line, bool):
                 raise UtilityHandler.InstanceBooleanError("line", self.line)
 
         if self.split:
+            # Raises an error when the split value is not a bool.
             if not isinstance(self.split, bool):
                 raise UtilityHandler.InstanceBooleanError("split", self.split)
 
         if self.end:
+            # Raises an error when the end value is not a bool.
             if not isinstance(self.end, bool):
                 raise UtilityHandler.InstanceBooleanError("end", self.end)
 
     def reset_symbol(self):
+        """
+        Resets the given symbols.
+        """
         for tree in self.trees:
+            # If a symbol is 'True', the default symbol will be used, else the current symbol will stay.
             if self.line:
                 self.line = "┃"
             else:
@@ -65,4 +94,5 @@ class ResetSymbol:
             else:
                 self.end = UtilityLibrarian.existing_symbols[tree]["end"]
 
+            # Sets the symbol values.
             UtilityLibrarian.existing_symbols.update({tree: {"line": self.line, "split": self.split, "end": self.end}})
